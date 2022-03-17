a39c721ccf716d17a9daa878ac55e946fdd1bc37
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-16 21:25:03 -0500

    actions

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import permissions
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from blog.models import Article
 from blog.permissions import IsOwnerOrReadOnly
 from blog.serializers import ArticleSerializer
+from rest_framework.decorators import  action
 
 
 class ArticleViewSet(ModelViewSet):
@@ -14,3 +16,10 @@ class ArticleViewSet(ModelViewSet):
     serializer_class = ArticleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,]
+
+    @action(detail=False)
+    def published_articles(self, request, *args, **kwargs):
+        articles = Article.objects.filter(is_published=True)
+        serializer = self.get_serializer_class()
+        serializer = serializer(articles, many=True)
+        return  Response(serializer.data, status=200)
--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from blog.models import Article
+from blog.serializers import ArticleSerializer
 
 User = get_user_model()
 
 from core.serializers import UserSerializer, CreateUserSerializer
-
+from rest_framework.decorators import action
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
@@ -14,3 +18,9 @@ class UserViewSet(ModelViewSet):
         if self.action == "create":
             return CreateUserSerializer
         return UserSerializer
+
+    @action(detail=True)
+    def user_articles(self, request, *args, **kwargs):
+        articles = Article.objects.filter(author_id=self.kwargs["pk"])
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data, status=200)
